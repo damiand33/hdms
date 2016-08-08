cfed0f625fd4e2cbe04fc1f26e43417f5d375d0c
Damian Dickerson <damian.dickerson@gmail.com>
2016-08-08 16:09:09 +0000

    Styled Navbar

--- app/controllers/worksheets_controller.rb
+++ app/controllers/worksheets_controller.rb
@@ -6,4 +6,15 @@ class WorksheetsController < ApplicationController
   def new
     @worksheet = Worksheet.new
   end
+
+  def create
+    Worksheet.create(worksheet_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def worksheet_params
+    params.require(:worksheet).permit(:piw_id, :submitted_by, :description)
+  end
 end
