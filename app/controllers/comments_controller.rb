49c68280a33fd80331bde7ac1a5355bf2de7f648
Damian Dickerson <damian.dickerson@gmail.com>
2016-08-10 17:02:17 +0000

    Added modal for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
   before_action :authenticate_user! # Requires that a user is logged in prior to adding a comment.
   def create
     @worksheet = Worksheet.find(params[:worksheet_id]) # Loads the worksheet the comment belongs to
-    @worksheet.comments.create(comment_params) # add comment to the Db
+    @worksheet.comments.create(comment_params.merge(user: current_user)) # add comment to the Db
     redirect_to worksheet_path(@worksheet) # send the user back to the worksheet details page
   end
 
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,10 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :worksheet
+
+  PRIORITY = {
+    'High' => 'High-code',
+    'Medium' => 'Medium-code',
+    'Low' => 'Low=code'
+  }
 end
