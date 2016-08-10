6c3298d3911a80ba2f4c1266cb65e5e676caa8d1
Damian Dickerson <damian.dickerson@gmail.com>
2016-08-10 14:52:30 +0000

    Comments are now saved to the Db

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,12 @@
+class CommentsController < ApplicationController
+  before_action :authenticate_user! # Requires that a user is logged in prior to adding a comment.
+  def create
+    @worksheet = Worksheet.find(params[:worksheet_id]) # Loads the worksheet the comment belongs to
+    @worksheet.comments.create(comment_params) # add comment to the Db
+    redirect_to worksheet_path(@worksheet) # send the user back to the worksheet details page
+  end
+
+  def comment_params
+    params.require(:comment).permit(:message, :priority) # pull data out of the comments form
+  end
+end
